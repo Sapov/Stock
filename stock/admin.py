07d773ce419837155d8c83f4f76aee791e74547d
Sapov <sapov@mail.ru>
2024-08-11 21:52:46 +0300

    add filter sortiable_by in admin

--- stock/admin.py
+++ stock/admin.py
@@ -4,4 +4,16 @@ from .models import Stock, Category, Equipment
 
 admin.site.register(Stock)
 admin.site.register(Category)
-admin.site.register(Equipment)
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Equipment._meta.fields]
+    list_editable = ["number"]
+    list_filter = ("name", "category", "stock", )
+    sortable_by = ["stock", "created_at", "category"]
+
+    class Meta:
+        model = Equipment
+
+
+admin.site.register(Equipment, EquipmentAdmin)
