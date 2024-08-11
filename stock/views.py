a99cdc22b98879459818a9944178b60364889b36
Sapov <sapov@mail.ru>
2024-08-11 19:47:20 +0300

    add viewsets

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Stock, Category, Equipment
+from .serializers import StockSerializer, CategorySerializer, EquipmentSerializer
 
-# Create your views here.
+
+class StockViewSet(viewsets.ViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class EquipmentViewSet(viewsets.ViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
