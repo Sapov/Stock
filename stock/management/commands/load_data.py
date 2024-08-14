3b3d8b42014bb66594bfa6750726c0d7b463dd6c
Sapov <sapov@mail.ru>
2024-08-14 23:11:59 +0300

    add requirements.txt

--- stock/management/commands/load_data.py
+++ stock/management/commands/load_data.py
@@ -1,8 +1,8 @@
 from django.core.management import BaseCommand
-from stock.models  import Stock, Category, Equipment
-
+from stock.models import Stock, Category, Equipment
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         # for data in
         ...
+
